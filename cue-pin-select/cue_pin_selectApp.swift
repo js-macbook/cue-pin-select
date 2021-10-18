157842e4d776de9a528c50d87913154038e0e204
Jeremy S <jeremystein14@gmail.com>
2021-10-18 13:09:33 -0400

    Initial Commit

---
+++ cue-pin-select/cue_pin_selectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cue_pin_selectApp.swift
+//  cue-pin-select
+//
+//  Created by Jeremy Stein on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct cue_pin_selectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
